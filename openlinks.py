1b2f45f85a2400c64a7f0a23b493972f0d72a25c
picaro <heinjame@gmail.com>
2019-02-25 23:14:50 +0630

    Update openlinks.py

--- openlinks.py
+++ openlinks.py
@@ -14,7 +14,7 @@ __________            __________                     __
 
 """
 inp1=raw_input("Enter Target URL for sublist:")
-url1="python ~/Sublist3r/sublist3r.py -d "+inp1+" -o "+inp1.split('.')[0]+".txt"
+url1="python ./Sublist3r/sublist3r.py -d "+inp1+" -o "+inp1.split('.')[0]+".txt"
 os.system(url1)
 print "[!]Subdomains ready! Checking valid url of not........"
 domain=""
@@ -29,7 +29,7 @@ with open(inp1.split('.')[0]+".txt",'r') as f:
 			r=requests.get(domain)
 			print "[+]Valid url:"+domain
 			print "[!]Fuzzing url..."
-			fuzz="python ~/Photon/photon.py -u "+domain
+			fuzz="python ./Photon/photon.py -u "+domain
 			os.system(fuzz)
 		except requests.exceptions.ConnectionError:
 			print "[-]Failed:"+domain
